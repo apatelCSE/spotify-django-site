f4aeb0c1f958ab8c97ab49c7d8130a5b4226bd54
Anika <anikapatel120@gmail.com>
2020-09-07 11:52:22 -0700

    security issue

--- spotifywebapp/settings.py
+++ spotifywebapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0j)q_41a_7+=@@eioef92iji$7&j+ubhf+#_yzf98#7cq9e7k9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
